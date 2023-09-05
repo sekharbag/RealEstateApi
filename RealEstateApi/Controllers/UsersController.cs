c7f6d905269ce41991c31162ca9b60f9507bc726
sekharbag <sekharb2000@gmail.com>
2023-09-06 01:38:14 +0530

    jwt token

--- RealEstateApi/Controllers/UsersController.cs
+++ RealEstateApi/Controllers/UsersController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RealEstateApi.Data;
 using RealEstateApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace RealEstateApi.Controllers
 {
@@ -9,8 +13,15 @@ namespace RealEstateApi.Controllers
     public class UsersController : ControllerBase
     {
        ApiDbContext _dbContext= new ApiDbContext();
+        private IConfiguration _configuration;
+        public UsersController(IConfiguration config)
+        {
+            _configuration = config;
+            //access to details of appsettings file
+        }
 
         [HttpPost("[Action]")]
+        //api/Users/Register
         public IActionResult Register([FromBody] User user)
         {
             //var userxist= _dbContext.users.Any(u=>u.Email==user.Email);
@@ -23,6 +34,34 @@ namespace RealEstateApi.Controllers
             }
             else
                 return BadRequest("already registered");
+
+        }
+        [HttpPost("[Action]")]
+        public IActionResult Login([FromBody] User user)
+        {
+            var currentuser = _dbContext.users.FirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+            if (currentuser == null) {
+                return
+                    StatusCode(StatusCodes.Status401Unauthorized);
+            }
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:key"]));
+            var credentials=   new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email)
+
+            };
+       
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                claims:claims,
+                expires:DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials);
+            var jwt=new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(jwt);
+
+
 
         }
 
--- RealEstateApi/Models/User.cs
+++ RealEstateApi/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Microsoft.EntityFrameworkCore;
 
 namespace RealEstateApi.Models
 {
